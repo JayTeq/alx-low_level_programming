ca3842ac9dd2d8ebdc5a1a035e16573c839f6eea
Jude <stephenjude108@gmail.com>
2022-12-09 06:09:56 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display triple digits and ,
+ *
+ * Return: Always 0 (successful)
+ */
+
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, j, k, l;
+
+    i = 0;
+    while (i < 1000)
+    {
+        j = i / 100; /* hundreds */
+        k = (i / 10) % 10; /* tens */
+        l = i % 100; /* units */
+        if (j < k && k < l)
+        {
+            putchar(l + '0');
+            putchar(k + '0');
+            putchar(j + '0');
+            if (i < 789)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        i++;
+    }
+    putchar('\n');
+
+    return (0);
+}
