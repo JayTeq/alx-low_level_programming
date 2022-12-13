19d206a5e82d8cd6d32558e8237f8ab25d2ccb54
Jude <stephenjude108@gmail.com>
2022-12-13 13:51:34 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 /**
-  * main - sum even fibonacci numbers under 4 million.
+  * main - print the first 98 fibonacci numbers.
   * Return: Nothing.
   */
 int main(void)
 {
-	unsigned long count, i, j, k, sums;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	i = sums = 0;
+	count = 0;
+	i = 0;
 	j = 1;
-	for (count = 0; count < 50; count++)
+	for (count = 1; count <= 91; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		if (k % 2 == 0 && k < 4000000)
-		{
-			sums += k;
-		}
+		printf("%lu, ", k);
 	}
-	printf("%lu\n", sums);
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
 	return (0);
 }
