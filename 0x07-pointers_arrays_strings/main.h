4f3cbfa5dc893960cb00e6e26f09ccc5573e0c44
Jude <stephenjude108@gmail.com>
2023-01-04 13:25:48 -0800

    new pull

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef ALX_H
 #define ALX_H
 
+int _putchar(char);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -13,4 +15,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif
